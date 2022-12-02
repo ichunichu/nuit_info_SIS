f4cca4bdb18009067b87283364ec983eaecbf020
JorianG <joriangouagout1@gmail.com>
2022-12-02 02:33:49 +0100

    card info

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,23 +1,34 @@
 import './style.css'
 
+
+
+
+
 export function Index(){
-    return (
+
+  return (
     <>
         <header>
             <img src="./logo.png" alt=""/>
         </header>
 
-        <div class="game">
-            <p class="game_desc"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error iure ducimus sunt animi rerum magni ea corrupti magnam provident, dolore ipsam sapiente similique mollitia voluptas incidunt eius quia modi molestiae.
+        <div className="game">
+            <p className="game_desc"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error iure ducimus sunt animi rerum magni ea corrupti magnam provident, dolore ipsam sapiente similique mollitia voluptas incidunt eius quia modi molestiae.
             Odio tempore eaque error consequatur dignissimos porro, pariatur consectetur rem ad aspernatur nisi iusto alias blanditiis laudantium, harum dolore natus tenetur perferendis hic ipsam exercitationem asperiores dolor quibusdam mollitia! Deserunt!
             Nihil alias, doloribus expedita assumenda quae incidunt, aliquam, itaque aliquid quidem illo aut dolor doloremque voluptatibus quam voluptatem. Minima nobis exercitationem facilis totam tenetur voluptatibus tempora dolor? Explicabo, unde iure!</p>
-            <button class="launch_game"> Jouer  </button>
+            <button className="launch_game"> Jouer  </button>
         </div> 
 
-        <div class="info">
+        <div className="info">
             <br/>
-            <ul class="info_list">
-                <li><p><strong>1</strong></p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
+                
+            <ul className="info_list">
+                <li>
+                    <p><strong>1</strong></p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
+                    Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p>
+                </li>
+                <li><p><strong>2</strong></p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
                 Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p></li>
                 <li><p><strong>1</strong></p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
                 Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p></li>
@@ -25,6 +36,11 @@ export function Index(){
                 Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p></li>
                 <li><p><strong>1</strong></p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
                 Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p></li>
+                <li><p><strong>1</strong></p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas voluptatibus amet quidem ut doloribus, ipsum deleniti, mollitia possimus consequuntur tempora harum quis enim quas! Eos ea deserunt mollitia iure rerum!
+                Commodi, molestiae! Quibusdam, nemo. Sit, voluptatum quibusdam temporibus cumque quia ex voluptas. Eum totam voluptatibus aliquam alias aliquid facere dolores autem, deleniti optio, unde labore iure repudiandae molestias sunt nemo?</p></li>
+
+                
+            
             </ul>
         </div>
     </>
